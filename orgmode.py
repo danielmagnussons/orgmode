ef12868d28a3993d3c76e11c0962b078bc4e3248
Daniel Magnusson <daniel.magnussons@gmail.com>
2018-03-12 18:28:54 +0100

    Update orgmode.py

--- orgmode.py
+++ orgmode.py
@@ -417,7 +417,6 @@ class OrgmodeCycleTodoCommand(sublime_plugin.TextCommand):
 
         # If the cursor is at the end of the view, return.
         if (sel.end() == sel.begin()) and (sel.end() == view.size()):
-            print("well")
             view.insert(edit, view.size(), "\n")
             view.show(view.size())
             return
